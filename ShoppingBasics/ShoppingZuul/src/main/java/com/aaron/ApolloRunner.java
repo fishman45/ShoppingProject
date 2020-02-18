b7d6627ef01a4a076fa82a4da4a82f62b776cbc9
361647454@qq.com <A.lflnl19901006>
2020-02-18 11:25:09 +0800

    添加apollo监听事件

---
+++ ShoppingBasics/ShoppingZuul/src/main/java/com/aaron/ApolloRunner.java
@@ -0,0 +1,33 @@
+package com.aaron;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: lfl
+ * @description:
+ * @date: Create in 2020/2/18 上午 10:29
+ */
+@Component
+@Slf4j
+public class ApolloRunner implements CommandLineRunner {
+
+    @ApolloConfig
+    private Config config;
+
+    @Override
+    public void run(String... args) throws Exception {
+        config.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(ConfigChangeEvent changeEvent) {
+                log.info("更改前端口值:" + changeEvent.getChange("server.port").getOldValue() +
+                        "更改后端口值:" + changeEvent.getChange("server.port").getNewValue());
+            }
+        });
+    }
+}
